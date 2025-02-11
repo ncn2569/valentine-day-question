2c43e660241e162d328bf055ad01a969fb16522b
ncn2569 <nguyencanhnguyen159357@gmail.com>
2025-02-11 14:34:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("showMessage").addEventListener("click", function () {
         button.textContent = "Click again!";
     } else {
         let message2 = document.createElement("p");
-        message2.textContent = "Nếu đồng ý gửi :v .Nếu không thì gửi :vvv và cảm ơn mày dã giành thời gian reply tin nhắn của tao cũng như xin lỗi đã làm phiền mày thời gian qua :))).";
+        message2.textContent = "Nếu đồng ý gửi :v , Nếu không thì gửi :vvv và cảm ơn mày dã giành thời gian reply tin nhắn của tao cũng như xin lỗi đã làm phiền mày thời gian qua :))).";
         message2.classList.add("loveMessage");
         document.querySelector(".container").appendChild(message2);
         button.disabled = true; // Vô hiệu hóa nút sau khi hiển thị thông điệp thứ hai
